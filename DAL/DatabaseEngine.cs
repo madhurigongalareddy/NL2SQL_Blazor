65a4fa0df098f863d43a30c3ac8f55554324b1d0
Deeba Anitharaj I <156064803+DeebaAnitharaj@users.noreply.github.com>
2025-06-13 16:56:21 +0530

    Added functionalites for multiple DB Support (#5)

---
+++ DAL/DatabaseEngine.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+public class DatabaseEngine
+{
+    private string _sqlConnString = "";
+    private string _postgreSqlConnString = "";
+
+    public DatabaseEngine(IConfiguration configuration)
+    {
+        _sqlConnString = configuration.GetConnectionString($"{QueryLanguage.MSSQL}_ConnString")
+                           ?? throw new ArgumentNullException(nameof(configuration), $"Connection string {QueryLanguage.MSSQL}_ConnString is null.");
+
+        _postgreSqlConnString = configuration.GetConnectionString($"{QueryLanguage.PostgreSQL}_ConnString")
+                           ?? throw new ArgumentNullException(nameof(configuration), $"Connection string {QueryLanguage.PostgreSQL}_ConnString is null.");
+    }
+
+    public QueryLanguage language { get; set; }
+
+    public enum QueryLanguage
+    {
+        MSSQL,
+        PostgreSQL
+    }
+
+    public async Task<DataTable> ExecuteQueryAsync(string sqlQuery)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(sqlQuery))
+            {
+                return new DataTable();
+            }           
+            using IDbConnection connection = language == QueryLanguage.MSSQL ? new SqlConnection(_sqlConnString) : new NpgsqlConnection(_postgreSqlConnString);
+            using IDbCommand command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+
+            var dataTable = new DataTable();
+            await ((dynamic)connection).OpenAsync();
+            using var reader = await ((dynamic)command).ExecuteReaderAsync();
+            dataTable.Load(reader);
+            return dataTable;
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (you can use any logging framework you prefer)
+            Console.WriteLine($"Error executing query: {ex.Message}");
+            return new DataTable();
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -7,11 +7,9 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddHttpClient();
 
-// Register the NlToSqlService as a scoped service
-builder.Services.AddScoped<NlToSqlService>(sp =>
-    new NlToSqlService(
-        sp.GetRequiredService<HttpClient>(),
-        sp.GetRequiredService<IConfiguration>()));
+// Register the NlQueryEngine as a scoped service
+builder.Services.AddScoped<NLQueryEngine>(sp => new NLQueryEngine (sp.GetRequiredService<HttpClient>(), sp.GetRequiredService<IConfiguration>()));
+builder.Services.AddScoped<DatabaseEngine>(sp => new DatabaseEngine(sp.GetRequiredService<IConfiguration>()));
 builder.Services.AddScoped<AuthenticationService>();
 builder.Services.AddScoped<UserService>();
 
