9f9484799cd05ebec479b61d6c8f9826659dce62
Deeba Anitharaj I <156064803+DeebaAnitharaj@users.noreply.github.com>
2025-06-19 19:18:33 +0530

    Fixed connection string for postgres and optimised schema (#16)

--- Services/NLQueryEngine.cs
+++ Services/NLQueryEngine.cs
@@ -96,24 +96,26 @@ public class NLQueryEngine
             return $"You are a highly skilled {dbServerName} Server expert with deep knowledge of US health insurance claims data for payer organizations. You write clean, optimized PostgreSQL queries " +
             $"using the provided database schema. \n  Your goal is to convert natural language requests into accurate, fully-formed PostgreSQL queries that run against a health " +
             $"claims database. Always select the most efficient approach. \n ### General Rules:\n - Use ANSI SQL where possible.\n - Use PostgreSQL-specific functions only when " +
-            $"needed (e.g., NOW(), AGE()).\n - Always join tables correctly using primary/foreign keys. \n - Always include WHERE clauses based on user filters.\n - " +
+            $"needed (e.g., NOW(), AGE()).\n - Join tables correctly using primary/foreign keys only when it is required. \n - Always include WHERE clauses based on user filters.\n - " +
             $"Use explicit column names (never SELECT *).\n - Apply aggregation functions (SUM, COUNT, AVG, etc.) when requested.\n - Format dates in 'YYYY-MM-DD' format.\n - " +
-            $"Avoid using reserved keywords as aliases.\n - Use LIMIT and OFFSET for top-N queries (if needed).\n - Always alias tables for clarity (e.g., ch for claims_header)." +
-            $"\n - NEVER use SUBSTR, LEFT, RIGHT, or any string manipulation functions to extract date parts from date columns. Use EXTRACT or date_trunc for date parts if needed.\n" +
+            $"Avoid using reserved keywords as aliases.\n - Use LIMIT and OFFSET for top-N queries (if needed).\n - Always alias tables for clarity." +
+            $"\n - NEVER use SUBSTR, SUBSTRING, LEFT, RIGHT,STRFTIME, DATE_FORMAT, or any string manipulation functions to extract date parts from date columns. Use EXTRACT or date_trunc for date parts if needed.\n" +
             $" - All date fields are stored as DATE or TIMESTAMP. You can directly compare date fields without conversions.\n - Always filter dates before aggregation whenever possible to improve query performance.\n " +
             $"### Database Schema:\n **{sqlSchema.Result}\n ### Output Format:\n Only output valid PostgreSQL code. Do not explain the query.Do not guess columns or tables if not in the " +
             $"provided schema. Do not add comments. Do not include any markdown formatting. Be aware the sql injection check mentioned in method Nl-to_sql.HasSqlInjectionPattern";
         }
         else// if (dbServerName.Equals("SQLServer", StringComparison.OrdinalIgnoreCase))
         {
-            return $"You are a highly skilled SQL Server expert with deep knowledge of US health insurance claims data for payer organizations. You write clean, optimized T-SQL queries " +
-           $"using the provided database schema. \n  Your goal is to convert natural language requests into accurate, fully-formed SQL Server queries that run against a health " +
-           $"claims database. Always select the most efficient approach. \n ### General Rules:\n - Use ANSI SQL where possible.\n - Use SQL Server-specific functions only when " +
-           $"needed (e.g., GETDATE(), DATEDIFF()).\n - Always join tables correctly using primary/foreign keys. \n - Always include WHERE clauses based on user filters.\n - " +
+            return $"You are a highly skilled Microsoft SQL Server (version 2022) expert with deep knowledge of US health insurance claims data for payer organizations. " +
+                $"You write clean, optimized T-SQL queries using the provided database schema. \n  Your goal is to convert natural language requests into accurate, fully-formed " +
+                $"Microsoft SQL Server queries that run against a health claims database. Always select the most efficient approach and the query should be simple as much as possible " +
+                $"and accurate  and there should not be any compilation errors\n ### General Rules:\n - Use ANSI SQL where possible.\n - Use only Microsoft SQL Server that supports version 2022 specific functions only when " +
+           $"needed (e.g., GETDATE(), DATEDIFF()).\n - Join tables correctly using primary/foreign keys only when it is required. \n - Always include WHERE clauses based on user filters.\n - " +
            $"Use explicit column names (never SELECT *).\n - Apply aggregation functions (SUM, COUNT, AVG, etc.) when requested.\n - Format dates in 'YYYY-MM-DD' format.\n - " +
-           $"Avoid using reserved keywords as aliases.\n - Use OFFSET ... FETCH for top-N queries (if needed).\n - Always alias tables for clarity (e.g., ch for Claims_Header)." +
-           $"\n - NEVER use SUBSTR, SUBSTRING, LEFT, RIGHT,STRFTIME, or any string manipulation functions to extract date parts.\n - All date fields are stored as DATE or DATETIME." +
-           $" You can directly compare date fields without conversions.\n - Always filter dates before aggregation whenever possible to improve query performance.\n " +
+           $"Avoid using reserved keywords as aliases.\n - Use OFFSET ... FETCH for top-N queries (if needed).\n - " +
+           //$"Always alias tables for clarity." +
+           $"\n - NEVER use SUBSTR, SUBSTRING, LEFT, RIGHT,STRFTIME, DATE_FORMAT, or any string manipulation functions to extract date parts and date filters\n - All date fields are stored as DATE or DATETIME." +
+           $" You can directly compare date fields without conversions.\n - Always filter data before aggregation whenever possible to improve query performance.\n " +
            $"### Database Schema:\n **{sqlSchema.Result}\n ### Output Format:\n Only output valid T-SQL code. Do not explain the query.Do not guess columns or tables if not in the provided schema." +
            $" Do not add comments. Do not include any markdown formatting. Be aware the sql injection check mentioned in method Nl-to_sql.HasSqlInjectionPattern";
         } 
